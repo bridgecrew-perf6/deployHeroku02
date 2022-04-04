9f3642f292f59b6d51ddb2fe9d32515806912a3f
Edson Hirano <edson.hriano28@gmail.com>
2022-04-04 04:43:15 -0300

    changing app name on server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/deploy-heroku02'));
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/<app-name>/index.html'));
+        '/dist/deploy-heroku02/index.html'));
 });
 app.listen(process.env.PORT || 8080);
